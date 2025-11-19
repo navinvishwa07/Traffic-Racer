4bd35302cb0078ef0589b4f74cae2e0cb1e640de
Ishitha Ilan <isithailan@gmail.com>
2025-11-19 13:50:29 +0530

    commit

---
+++ src/Constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+const float LANE_LEFT = 215.f;
+const float LANE_MIDDLE = 400.f;
+const float LANE_RIGHT = 585.f;
+
+const float PLAYER_Y = 500.f;
+const float LANE_SMOOTHING = 10.0f; 
